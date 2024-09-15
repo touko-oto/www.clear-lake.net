16abf7eea922855dbd6e6e7232bf836c7143dbd1
透湖 <68800986+touko-oto@users.noreply.github.com>
2024-09-16 01:28:52 +0900

    edit metadata

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['images.microcms-assets.io'],
+        domains: [ "images.microcms-assets.io"],
         loader: 'default',
         path: '/_next/image',
         deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,18 @@ import { PageFooter } from './layouts/PageFooter';
 const noto_sans_jp = Noto_Sans_JP({ subsets: ["latin"]});
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://www.clear-lake.net'),
+  generator: 'Vercel',
+  applicationName: 'clear-lake.net',
   title: "clear-lake.net",
   description: "透湖の詳細プロフィール, SNSアカウントまとめ",
-  keywords: "透湖, プロフィール, リンク集, アニメ, Blog, SNS, Twitter, Misskey, GitHub, Bluesky, Thread, YouTube, PixAI",
+  keywords: ['透湖', 'プロフィール', 'リンク集', 'アニメ', 'Blog', 'SNS', 'Twitter', 'Misskey', 'GitHub', 'Bluesky', 'Thread', 'YouTube', 'PixAI'],
+  authors: [{ name: '透湖', url: 'https://clear-lake.net/' }],
+  creator: '透湖',
+  publisher: '星名サナエ',
+  openGraph: {
+    images: '/opengraph-image.png',
+  },
 };
 
 export default function RootLayout({
